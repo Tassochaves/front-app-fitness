aa02415fe0463dadfa90cf1a579bf8d94c9fa41d
Tassochaves <tassogt@hotmail.com>
2024-10-25 22:38:21 -0300

    Feat: implementa grafico de linha para mostrar estatisticas

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { EstatisticaService } from '../../service/estatistica.service';
 import { GraficosService } from '../../service/graficos.service';
 import { CategoryScale, Chart } from 'chart.js/auto';
+import { DatePipe } from '@angular/common';
 
 Chart.register(CategoryScale);
 
@@ -11,7 +12,8 @@ Chart.register(CategoryScale);
   standalone: true,
   imports: [SharedModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  providers: [DatePipe]
 })
 export class DashboardComponent {
 
@@ -26,11 +28,20 @@ export class DashboardComponent {
   @ViewChild('graficoDeLinhaAtividades')
   private graficoDeLinhaAtividadeRef!: ElementRef;
 
-  constructor(private estatisticaService: EstatisticaService, private graficoService: GraficosService){}
+  constructor(
+    private estatisticaService: EstatisticaService,
+    private graficoService: GraficosService,
+    private datePipe: DatePipe){}
 
   ngOnInit(): void {
     this.obterEstatisticas();
     this.obterDadosGrafico();
+  }
+
+  ngAfterViewInit(): void {
+    if(this.treinos && this.atividades){
+      this.criarGraficoDeLinha();
+    }
   }
 
   obterEstatisticas(){
@@ -55,13 +66,64 @@ export class DashboardComponent {
     const atividadeContext = this.graficoDeLinhaAtividadeRef.nativeElement.getContext('2d');
 
     new Chart(treinoContext, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
+        labels: this.treinos.map((dados: {data: any;}) => this.datePipe.transform(dados.data, 'dd/MM')),
+        datasets: [
+          {
+          label: 'Calorias queimadas',
+          data: this.treinos.map((dados: {caloriasQueimadas: any;}) => dados.caloriasQueimadas),
+          fill: false,
+            borderWidth: 1,
+            backgroundColor: 'rgba(80, 200, 120, 0.6)',
+            borderColor: 'rgba(0, 100, 0, 1)',
+        },
+        {
+          label: 'Duração',
+          data: this.treinos.map((dados: {duracao: any;}) => dados.duracao),
+          fill: false,
+            borderWidth: 1,
+            backgroundColor: 'rgba(120, 180, 200, 0.6)',
+            borderColor: 'rgba(0, 100, 150, 1)',
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    new Chart(atividadeContext, {
+      type: 'line',
+      data: {
+        labels: this.atividades.map((dados: {data: any;}) => this.datePipe.transform(dados.data, 'dd/MM')),
+        datasets: [
+          {
+          label: 'Calorias queimadas',
+          data: this.atividades.map((dados: {caloriasQueimadas: any;}) => dados.caloriasQueimadas),
+          fill: false,
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 100, 100, 0.6)',
+            borderColor: 'rgba(255, 0, 0, 1)',
+        },
+        {
+          label: 'Passos',
+          data: this.atividades.map((dados: {passos: any;}) => dados.passos),
+          fill: false,
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 180, 120, 0.6)',
+            borderColor: 'rgba(255, 100, 0, 1)',
+        },
+        {
+          label: 'Distancia',
+          data: this.atividades.map((dados: {distancia: any;}) => dados.distancia),
+          fill: false,
+            borderWidth: 1,
+            backgroundColor: 'rgba(255, 200, 200, 0.6)',
+            borderColor: 'rgba(255, 0, 100, 1)',
         }]
       },
       options: {
